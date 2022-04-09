c20a53bf1741bd1bd17a0dc1b7b19af560face91
Jorge Cardozo <paratrabajosydemas@gmail.com>
2022-04-09 08:40:41 -0500

    segundo commit despues de un gran tiempo! plus otra linea

--- holaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
+++ holaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -6,6 +6,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundoooooooo!");
         System.out.println("Texto para practicar git");
-        System.out.println("Otra linea más");
+        System.out.println("Otra linea más.");
+        System.out.println("No suggestions");
     }
 }
